79d8b299437f199d04ab591238fa4e80c33e9a86
jeffumbricht <jeff@sandstormdesign.com>
2021-07-31 13:38:23 -0500

    Blocked out sections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import './App.css';
+
 function App() {
   return (
-    <div className="app">
-    </div>
+    <main className="app">
+      <section className="sidebar">
+        <header>
+          <button>
+            Search for places
+          </button>
+          <button>
+            By location
+          </button>
+        </header>
+
+        <article>Today's Weather</article>
+
+      </section>
+      <section>
+        5 day forecast
+        <article>Today's Weather</article>
+        <article>Tomorrow's Weather</article>
+
+        Today's highlights
+        <article>Weather stat</article>
+        <article>Weather stat</article>
+      </section>
+
+    </main>
   );
 }
 
