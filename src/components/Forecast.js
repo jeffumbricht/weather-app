b8d18f957d6e73c9d783f87ef78f896148ebe65c
jeffumbricht <jeff@sandstormdesign.com>
2021-08-05 21:40:58 -0500

    Grid cleanup

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -6,16 +6,18 @@ function Forecast({forecast}) {
   return (
     <section className="forecast">
       <h2>{forecast.length} Day Forecast</h2>
-      <div>
+      <div className="forecast-row">
         {forecast.map(item =>
-          <WeatherCard
-            key={item.id}
-            date={item.applicable_date}
-            forecast="LightCloud"
-            high={item.max_temp}
-            low={item.min_temp}
-            weatherStateAbbr={item.weather_state_abbr}/>
-        )}
+          <div>
+            <WeatherCard
+              key={item.id}
+              date={item.applicable_date}
+              forecast="LightCloud"
+              high={item.max_temp}
+              low={item.min_temp}
+              weatherStateAbbr={item.weather_state_abbr}/>
+          </div>
+          )}
       </div>
     </section>
   );
--- src/components/HighlightCard.js
+++ src/components/HighlightCard.js
@@ -2,12 +2,12 @@ import './HighlightCard.scss';
 
 function HighlightCard({title, stat, statSuffix}) {
   return (
-    <div className="highlight-card">
+    <article className="highlight-card">
       <h3>{title}</h3>
       <div className="highlight-stat">
         {stat} <span className="highlight-stat-suffix">{statSuffix}</span>
       </div>
-    </div>
+    </article>
   );
 }
 
